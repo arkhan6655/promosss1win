d376025cc23d836b43bb2ed6654f79e62222feee
arkhan6655 <mdashikuzzaman000648@gmail.com>
2026-03-26 15:52:19 +0600

    Delete configs directory

--- configs/config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyAAAkjOkTtB0m9FYLjw9kmdkws4a5jkN2A",
-    authDomain: "win-stats.firebaseapp.com",
-    databaseURL: "https://win-stats-default-rtdb.firebaseio.com",
-    projectId: "win-stats",
-    storageBucket: "win-stats.firebasestorage.app",
-    messagingSenderId: "590275471925",
-    appId: "1:590275471925:web:a7035ab6e493a1b8c89f7c"
-};
